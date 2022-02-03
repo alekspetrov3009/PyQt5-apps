e1a1e8814e38b09b407771c4fa2a12bd32aaa598
alekspetrov3009 <59197302+alekspetrov3009@users.noreply.github.com>
2022-02-03 15:38:15 +0300

    from 1 to 6th lesson

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pickle
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QDate
@@ -9,27 +10,83 @@ form.setupUi(window)
 window.show()
 
 
+def save_to_file():
+    global start_date, calc_date, description
+    #start_date = QDate(2022, 2, 1)
+    data_to_save = {"start": start_date, "end": calc_date, "desc" : description}
+    file1 = open("config.txt", "wb")
+    pickle.dump(data_to_save, file1)
+    file1.close()
+                
+
+def read_from_file():
+    global start_date, calc_date, description, now_date
+    try:
+        file1 = open("config.txt", "rb")
+        data_to_load = pickle.load(file1)
+        file1.close()
+        start_date = data_to_load["start"]
+        calc_date = data_to_load["end"]
+        description = data_to_load["desc"]
+        print(start_date.toString('dd-MM-yy'), calc_date.toString('dd-MM-yy'), description)
+        form.calendarWidget.setSelectedDate(calc_date)
+        form.dateEdit.setDate(calc_date)
+        form.plainTextEdit.setPlainText(description)
+        delta_days_left = start_date.daysTo(now_date) #прошло дней
+        delta_days_right = now_date.daysTo(calc_date) #осталось дней
+        days_total = start_date.daysTo(calc_date) #всего дней
+        print("FFFFFFF", delta_days_left, delta_days_right, days_total)
+        procent = delta_days_left * 100 / days_total
+        print(procent)
+        form.progressBar.setProperty("value", procent)
+    except:
+        print("Не могу прочитать файл")
+
+
+
 def on_click():
-    print(form.plainTextEdit.toPlainText())
-    print(form.dateEdit.dateTime().toString('dd-MM-yy'))
+    global calc_date, description, start_date
+    start_date = now_date
+    calc_date = form.calendarWidget.selectedDate()
+    description = form.plainTextEdit.toPlainText()
+    #print(form.plainTextEdit.toPlainText())
+    #print(form.dateEdit.dateTime().toString('dd-MM-yy'))
+    save_to_file()
     #print('clicked')
     #print(form.calendarWidget.selectedDate().toString('dd-MM-yy'))
     #date = QDate(2022, 9, 30)
     #form.calendarWidget.setSelectedDate(date)
     
 def on_click_calendar():
-    print(form.calendarWidget.selectedDate().toString('dd-MM-yy'))
+    global start_date, calc_date
+    #print(form.calendarWidget.selectedDate().toString('dd-MM-yy'))
     form.dateEdit.setDate(form.calendarWidget.selectedDate())
+    calc_date = form.calendarWidget.selectedDate()  
+    delta_days = start_date.daysTo(calc_date) #расчет кол-ва дней
+    print(delta_days)
+    form.label_3.setText("До наступления события осталось: %s дней" % delta_days)
     
 def on_dateedit_change():
-    print(form.dateEdit.dateTime().toString('dd-MM-yy'))
+    global start_date, calc_date
+    #print(form.dateEdit.dateTime().toString('dd-MM-yy'))
     form.calendarWidget.setSelectedDate(form.dateEdit.date())
+    calc_date = form.dateEdit.date()  
+    delta_days = start_date.daysTo(calc_date)
+    print(delta_days)
+    form.label_3.setText("До наступления события осталось: %s дней" % delta_days)
 
 form.pushButton.clicked.connect(on_click)
 form.calendarWidget.clicked.connect(on_click_calendar)
 form.dateEdit.dateChanged.connect(on_dateedit_change)
 
-#form.label.setText('asdasdsa')
 
+start_date = form.calendarWidget.selectedDate()
+now_date = form.calendarWidget.selectedDate()
+calc_date = form.calendarWidget.selectedDate()
+description = form.plainTextEdit.toPlainText()
+read_from_file()
+
+form.label.setText("Трекер события от %s " % start_date.toString('dd-MM-yy'))
+on_click_calendar()
 
 app.exec_()
